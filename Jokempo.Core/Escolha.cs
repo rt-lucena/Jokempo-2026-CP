f2827b2c82e9e00e27058fe659afed7ace47eaf2
Rafael T Lucena <teofilo.rafinha@gmail.com>
2026-03-16 23:46:47 -0300

    class library e winforms do jokempo

---
+++ Jokempo.Core/Escolha.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jokempo.Core;
+
+public enum Escolha
+{
+    Pedra = 0,
+    Papel = 1,
+    Tesoura = 2
+}
